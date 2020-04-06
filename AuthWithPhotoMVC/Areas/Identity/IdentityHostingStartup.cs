12d37a086df2c3ec62f4d460d15626d3e522671e
Scott Hunter <scothu@microsoft.com>
2020-04-05 18:35:12 -0700

    Add project files.

---
+++ AuthWithPhotoMVC/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using AuthWithPhotoMVC.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(AuthWithPhotoMVC.Areas.Identity.IdentityHostingStartup))]
+namespace AuthWithPhotoMVC.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
